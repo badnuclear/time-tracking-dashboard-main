d773a05eacf60211c90762f57a697154648623e6
badnuclear <86408078+badnuclear@users.noreply.github.com>
2022-07-25 14:31:42 +0900

    2022.07.25 세이브

---
+++ time.js
@@ -0,0 +1,6 @@
+const dailyBtn = document.querySelector("#daily-btn");
+const weeklyBtn = document.querySelector("#weekly-btn");
+const monthlyBtn = document.querySelector("#monthly-btn");
+
+const hours = document.querySelector(".hours");
+const lastTime = document.querySelector(".last-time");
