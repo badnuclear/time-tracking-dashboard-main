61e408005a79f63b8430c6eaec5069a15503a666
badnuclear <86408078+badnuclear@users.noreply.github.com>
2022-07-26 18:21:40 +0900

    2022.07.26(화) css 마무리

--- time.js
+++ time.js
@@ -4,3 +4,65 @@ const monthlyBtn = document.querySelector("#monthly-btn");
 
 const hours = document.querySelector(".hours");
 const lastTime = document.querySelector(".last-time");
+
+function dailyData() {
+  fetch(
+    "https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.daily.current = "hrs";
+        lastTime[i].innerHTML =
+          "Last Daily - " + data[i].timeframes.daily.previous + "hrs";
+      });
+    });
+}
+
+function weekendData() {
+  fetch(
+    "https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.weekly.current = "hrs";
+        lastTime[i].innerHTML =
+          "Last Daily - " + data[i].timeframes.weekly.previous + "hrs";
+      });
+    });
+}
+
+function monthlyData() {
+  fetch(
+    "https://raw.githubusercontent.com/christopherjael/time-tracking-dashboard--solution/master/data.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      hours.forEach((e, i) => {
+        hours[i].innerHTML = data[i].timeframes.monthly.current = "hrs";
+        lastTime[i].innerHTML =
+          "Last Daily - " + data[i].timeframes.monthly.previous + "hrs";
+      });
+    });
+}
+
+dailyBtn.addEventListener("click", () => {
+  dailyBtn.classList.remove("text-btn");
+  weeklyBtn.classList.add("text-btn");
+  monthlyBtn.classList.add("text-btn");
+  dailyData();
+});
+
+weeklyBtn.addEventListener("click", () => {
+  dailyBtn.classList.remove("text-btn");
+  weeklyBtn.classList.add("text-btn");
+  monthlyBtn.classList.add("text-btn");
+  weekendData();
+});
+monthlyBtn.addEventListener("click", () => {
+  dailyBtn.classList.remove("text-btn");
+  weeklyBtn.classList.add("text-btn");
+  monthlyBtn.classList.add("text-btn");
+  monthlyData();
+});
